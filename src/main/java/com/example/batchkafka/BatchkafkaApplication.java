d52294011a0a4daf9d2ddb06213103e9ca73f6c5
Luiz Pinheiro <luizpinheiro.rs@gmail.com>
2019-08-31 15:29:44 -0300

    first commit

---
+++ src/main/java/com/example/batchkafka/BatchkafkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.batchkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchkafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchkafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/batchkafka/BatchkafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.batchkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchkafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
